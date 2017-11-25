3d3361d06397697cde344ecb02eb6781920fb1fa
robertsbd <roberts.b.d@gmail.com>
2017-11-25 18:31:37 +0000

    init commmit

---
+++ src/trajectory/core.clj
@@ -0,0 +1,7 @@
+(ns trajectory.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/trajectory/core_test.clj
@@ -0,0 +1,7 @@
+(ns trajectory.core-test
+  (:require [clojure.test :refer :all]
+            [trajectory.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
